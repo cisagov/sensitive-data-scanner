5a5fca77ccc55b433b5483e634b9034efc94a38d
Felddy <markf@geekpad.com>
2021-06-17 11:41:19 -0400

    Improve command line argument parsing and usage output.
    Add ability to skip to a specific line number at startup.
    Move threads to an optional argument.
    Add startup and completion log output.

--- src/run-gitleaks.sh
+++ src/run-gitleaks.sh
@@ -7,19 +7,82 @@ set -o nounset
 # set -o errexit
 set -o pipefail
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ${0} <threads> <github-urls-file>"
-    exit 255
+USAGE=$(cat << EOF
+Run gitleaks over multiple GitHub repositories.
+
+Usage:
+  ${0} [options] <github-urls-file>
+  ${0} (-h | --help)
+
+Options:
+  -h --help                    Show this message.
+  -s --skipto=<line-num>       Skip to line number.
+  -t --threads=<thread-count>  Number of gitleak threads to run [default: 2]
+
+
+EOF
+)
+
+# Positional parameters
+PARAMS=""
+
+# Defaults
+SKIP_TO_LINE=0
+THREADS=2
+
+# Parse command line arguments
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      echo "${USAGE}" >&2
+      exit 0
+      ;;
+    -s|--skipto)
+      shift
+      SKIP_TO_LINE=$1
+      shift
+      ;;
+    -t|--threads)
+      shift
+      THREADS=$1
+      shift
+      ;;
+    -*) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+
+if [ "$#" -ne 1 ]; then
+    echo "${USAGE}"
+    exit 1
 fi
+
+URL_FILE=$1
 
 timestamp=$(date +%Y-%m-%d-%H%M%S)
 
-i=0
+echo ">>>> Starting scan at ${timestamp} using ${THREADS} threads." >&2
+
+line_number=0
 while read -r url; do
-  i=$((i + 1))
-  >&2 echo ">>>> Processing URL #${i}: ${url}"
+  line_number=$((line_number + 1))
+  if (( line_number < SKIP_TO_LINE )); then
+    continue
+  fi
+  echo ">>>> Processing URL #${line_number}: ${url}" >&2
   output_dir="${url#*github.com/}"
   output_file=${output_dir}/${timestamp}.serif.json
   mkdir -p "${output_dir}"
-  gitleaks --threads="${1}" --format=sarif --report="${output_file}" --repo-url "${url}"
-done < "$2"
+  gitleaks --threads="${THREADS}" --format=sarif --report="${output_file}" --repo-url "${url}"
+done < "$URL_FILE"
+
+echo ">>>> All scans completed." >&2
